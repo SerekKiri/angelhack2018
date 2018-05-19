3d5f302edc642262c14621233cca792723897db1
alufers <alufers@wp.pl>
2018-05-19 17:36:21 +0200

    Added various fields

---
+++ frontend/docugram/components/fields/CheckboxField.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-checkbox v-on="$listeners" :value="value" :disabled="disabled" :label="label" />
+</template>
+
+<script>
+export default {
+  props: ['value', 'label', 'disabled']
+}
+</script>
+
+<style>
+</style>
