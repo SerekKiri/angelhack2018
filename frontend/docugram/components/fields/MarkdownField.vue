77b0e60b2e8769cd7194cd771c7d057d6907103a
alufers <alufers@wp.pl>
2018-05-19 18:55:46 +0200

    Added basic template editor

--- frontend/docugram/components/fields/MarkdownField.vue
+++ frontend/docugram/components/fields/MarkdownField.vue
@@ -1,13 +1,18 @@
 <template>
-    
+    <div v-html="renderedHtml" />
 </template>
 
 <script>
+import marked from 'marked'
 export default {
-
+  props: ['content'],
+  computed: {
+    renderedHtml() {
+        return marked(this.content)
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
