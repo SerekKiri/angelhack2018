44de4f53595bde7f61bf715929e73dc0faa08cf1
alufers <alufers@wp.pl>
2018-05-19 14:09:41 +0200

    Prepared stuff for apollo on frontend

--- frontend/docugram/nuxt.config.js
+++ frontend/docugram/nuxt.config.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   /*
   ** Headers of the page
@@ -8,17 +7,23 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js + Vuetify.js project' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt.js + Vuetify.js project'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+      }
     ]
   },
   plugins: ['~/plugins/vuetify.js'],
-  css: [
-    '~/assets/style/app.styl'
-  ],
+  css: ['~/assets/style/app.styl'],
   /*
   ** Customize the progress bar color
   */
@@ -27,14 +32,12 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    vendor: [
-      '~/plugins/vuetify.js'
-    ],
+    vendor: ['~/plugins/vuetify.js'],
     extractCSS: true,
     /*
     ** Run ESLint on save
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
@@ -43,6 +46,14 @@ module.exports = {
           exclude: /(node_modules)/
         })
       }
+    }
+  },
+  modules: ['@nuxtjs/apollo'],
+
+  // Give apollo module options
+  apollo: {
+    clientConfigs: {
+      default: '~/apollo/client-configs/default.js'
     }
   }
 }
