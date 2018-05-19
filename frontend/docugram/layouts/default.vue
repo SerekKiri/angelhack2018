cd854e17e3f6c5093b07519d84728f9322f00609
Unknown <kacper.potyrala@gmail.com>
2018-05-19 12:45:15 +0200

    Home button

--- frontend/docugram/layouts/default.vue
+++ frontend/docugram/layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
   <v-app dark>
     <v-toolbar fixed app :clipped-left="clipped">
+      <v-btn to="/">
+        <v-icon>home</v-icon>HOME
+      </v-btn>
+      <v-spacer></v-spacer>
       <v-btn flat to='/user'>Login</v-btn>
     </v-toolbar>
     <v-content>
