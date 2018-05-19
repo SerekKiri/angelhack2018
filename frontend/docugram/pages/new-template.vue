bde23d2c98b6cefc4ec0883d479dbe170981bed7
alufers <alufers@wp.pl>
2018-05-19 15:05:45 +0200

    Added new template page

---
+++ frontend/docugram/pages/new-template.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 class="subheader">Create a document template</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  layout: 'dashboard'
+}
+</script>
+
+<style>
+</style>
