6f2882c0dbe74fc87b411f7a384c4d09b7236275
Unknown <kacper.potyrala@gmail.com>
2018-05-19 12:38:02 +0200

    Auto stash before merge of "master" and "origin/master"

--- frontend/docugram/pages/user.vue
+++ frontend/docugram/pages/user.vue
@@ -2,13 +2,11 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
       <v-card>
-        <v-card-title class="headline">Welcome to the Docugram</v-card-title>
+        <v-card-title class="headline">Welcome new User!</v-card-title>
         <v-card-text>
-          Here you can create forumlars for trips and more!
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="primary" flat nuxt to="/user">Continue</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
